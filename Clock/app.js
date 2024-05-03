410a7e277f8387a67c2b42ed09523df4c98983a5
Abdullah <153812478+Abdullah3122004@users.noreply.github.com>
2024-05-04 04:18:00 +0500

    Update app.js

--- Clock/app.js
+++ Clock/app.js
@@ -2,10 +2,14 @@ var hours=document.getElementById(hour);
 var mins=document.getElementById(minute);
 var seconds=document.getElementById(second);
 
-
 setInterval(function(){
     var currentDate = new Date();
     var currentHour = currentDate.getHours();
+    if(currentDate.getHours()<12){
+    am_pm.innerText="am"
+    }else{
+        am_pm.innerText="pm"
+    }
     hour.innerText = currentDate.getHours();
     minute.innerText = currentDate.getMinutes();
     second.innerText = currentDate.getSeconds();
